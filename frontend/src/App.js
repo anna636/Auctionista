0f50784fe3b3526c9c3c6d86f4608329a5fbc193
TiredBirb <72065138+anna636@users.noreply.github.com>
2021-10-23 11:01:47 +0200

    Hot fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,20 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 function App() {
-  return <Router className="App">
-    <Navbar />
-    <main>
-    <p>heloo</p>
-    
-    </main>
-    <footer>
-   <Footer/>
-    </footer>
-  </Router>;
+  return (
+    <Router className="App">
+      <Navbar />
+      <main>
+        <Route path="/" exact component={Home} />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </Router>
+  );
 }
 
 export default App;
