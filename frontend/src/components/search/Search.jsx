116cee5557cbedbdaf1af386f1639da1f01e22af
nicolebradleey <nicole_bradley94@hotmail.com>
2021-10-25 16:02:45 +0200

    Api.rest

--- frontend/src/components/search/Search.jsx
+++ frontend/src/components/search/Search.jsx
@@ -16,10 +16,12 @@ const Search = () => {
     const filteredAuctionItems = [];
     for (let i = 0; i < auctionItems.length; i++) {
       let auctionItemTitle = auctionItems[i].title.toLowerCase();
+      // word array 1 och word array 2 som i foodharvester
       if (auctionItemTitle === userInput) {
         filteredAuctionItems.push(auctionItems[i]);
       }
     }
+    console.log(filteredAuctionItems)
   }
 
   return (
