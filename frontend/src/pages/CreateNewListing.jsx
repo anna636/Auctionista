7991a02b9b822a6f49a8665cc43b9e45a1235e7f
TiredBirb <72065138+anna636@users.noreply.github.com>
2021-10-25 10:41:14 +0200

    Made page template

--- frontend/src/pages/CreateNewListing.jsx
+++ frontend/src/pages/CreateNewListing.jsx
@@ -4,7 +4,37 @@ function CreateNewListing() {
   return (
     <div className="newListingWrapper" style={styles.wrapper}>
       <div className="form" style={styles.form}>
-        
+        <div className="inputs" style={styles.inputs}>
+          <div style={styles.inputInside} className="inputInside">
+            <label htmlFor="">Title</label>
+            <input type="text" style={styles.input} />
+          </div>
+
+          <div style={styles.inputInside} className="inputInside">
+            <label htmlFor="">Reservation price</label>
+            <input type="number" style={styles.input} />
+          </div>
+
+          <div style={styles.inputInside} className="inputInside">
+            <label htmlFor="">Start price</label>
+            <input type="number" style={styles.input} />
+          </div>
+          <div style={styles.inputInside} className="inputInside">
+            <label htmlFor="">Description</label>
+            <textarea
+              name="descrition"
+              id=""
+              cols="30"
+              rows="5"
+              style={styles.textArea}
+            ></textarea>
+          </div>
+        </div>
+        <div className="submitBtn">
+          <button className="postNewListing" style={styles.btn}>
+            Post new listing
+          </button>
+        </div>
       </div>
       <div className="coolImg">
         <img
@@ -31,5 +61,52 @@ const styles = {
   form: {
     width: "100%",
     backgroundColor: "rgb(226, 89, 55)",
+    display: "flex",
+    flexDirection: "column",
+    padding: "5vw",
+    gap: "15vh",
+  },
+  inputs: {
+    backgroundColor: "rgb(226, 89, 55)",
+    borderRaidus: "30%",
+
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    gap: "8vh",
+    color: "white",
+  },
+
+  input: {
+    borderWidth: "0 0 1px 0" /* top right bottom left */,
+    borderStyle: "none none solid none",
+    borderColor: "#f00 #0f0 white #ff0",
+    backgroundColor: "rgb(226, 89, 55)",
+    color: "white",
+  },
+
+  inputInside: {
+    display: "grid",
+    gridTemplateColumns: "20vw 25vw",
+    gap: "5vh",
+    justifyContent: "center",
+    width: "100%",
+    textAlign: "left",
+    fontSize:"1.1em"
+  },
+
+  textArea: {
+    borderRaidus: "20%",
+    border: "none",
+  },
+  btn: {
+    border: "none",
+    borderRadius: "10px",
+    padding: "0.5vw",
+    fontSize: "1.2em",
+    backgroundColor: "black",
+    color:"white"
   },
 };
