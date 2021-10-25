bc1bc71faa879ffcbece22b4c69b517ad09593ce
TiredBirb <72065138+anna636@users.noreply.github.com>
2021-10-25 19:26:13 +0200

    Fixed that primary images can be seen now

--- frontend/src/components/AuctionItemCard.jsx
+++ frontend/src/components/AuctionItemCard.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import { useHistory } from "react-router-dom";
 
 
 
 
+
 const AuctionItemCard = (props) => {
   const history = useHistory();
-  
+  const [primaryImgPath, setPrimaryImgPath]=useState("")
+
+   useEffect(() => {
+    let imagePathArr = props.props.images.split(",");
+     let primaryImgPath = imagePathArr[props.props.primaryImgIndex];
+     setPrimaryImgPath(primaryImgPath)
+   }, []);
+
+
+
   function redirect() {
     history.push("/details/"+ props.props.id)
   }
@@ -32,8 +42,9 @@ const AuctionItemCard = (props) => {
         </div>
         <img
           style={styles.img}
-          src="https://us.123rf.com/450wm/khmel/khmel1611/khmel161100006/67107397-three-budgies-are-in-the-roost.jpg?ver=6"
+          src={primaryImgPath}
           alt=""
+          
         />
       </div>
       <div className="title" style={styles.title}>
