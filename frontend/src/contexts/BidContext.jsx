68e5677e451d7aabbcc7462cff9297407b0eef14
JacobLexhag <72065984+JacobLexhag@users.noreply.github.com>
2021-10-27 13:46:45 +0200

    created bidContext
    Co-Authored-By: Tobias Östergren <45110274+volture1@users.noreply.github.com>

---
+++ frontend/src/contexts/BidContext.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { createContext, useContext, useState, useEffect } from "react";
+
+const BidContext = createContext();
+
+export const useBidContext = () => {
+  return useContext(BidContext);
+};
+
+
+const BidProvider = (props) => {
+
+  const [bids, setBids] = useState([])
+
+  useEffect(() => {
+      fetchAllBids()
+      }, []);
+   
+  const fetchAllBids = async () => { 
+    let response = await fetch("/rest/bids")
+    setBids(await response.json())
+  };
+
+  const fetchBid = async (id) => {
+    let res = await fetch("/rest/bids/" + id)
+    try {
+      let fetchedItem = await res.json()
+      console.log("From fetchBid: ", fetchedItem)
+      return fetchedItem;
+    }
+    catch {
+      console.log("No item found")
+    }
+  }
+
+  const postNewBid = async (itemToPost) => {
+    let response = await fetch("/rest/bids", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(itemToPost),
+    });
+   console.log(await response.json())
+    return response
+   
+  }
+
+  const values = {
+    postNewBid,
+    bids,
+    fetchAllBids,
+    fetchBid
+  };
+
+  return (
+    <BidContext.Provider value={values}>
+      {props.children}
+    </BidContext.Provider>
+  );
+};
+
+export default BidProvider;
