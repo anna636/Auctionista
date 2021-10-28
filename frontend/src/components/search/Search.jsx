c4349a286a350dc78876e32e79271417da73d26f
nicolebradleey <nicole_bradley94@hotmail.com>
2021-10-28 15:38:06 +0200

    CSS fix

--- frontend/src/components/search/Search.jsx
+++ frontend/src/components/search/Search.jsx
@@ -5,8 +5,7 @@ import { Popover, OverlayTrigger } from "react-bootstrap";
 const Search = () => {
   const [userInput, setUserInput] = useState();
   const { fetchAuctionItemByTitle } = useAuctionItem();
-  const [ popoverMessage, setPopoverMessage ] = useState(false)
-
+  const [popoverMessage, setPopoverMessage] = useState(false);
 
   function handleSearch(event) {
     event.preventDefault();
@@ -14,32 +13,28 @@ const Search = () => {
     setUserInput("");
   }
 
-
-const getAuctionItemByTitle = async () => {
-  let fetchedItem = await fetchAuctionItemByTitle(userInput);
-  if (fetchedItem) {
-    console.log("fetched item: ", fetchedItem)
-    setPopoverMessage(false)
-  }
-  else {
-    console.log("No items found")
-    setPopoverMessage(true)
-  }
-
+  const getAuctionItemByTitle = async () => {
+    let fetchedItem = await fetchAuctionItemByTitle(userInput);
+    if (fetchedItem) {
+      console.log("fetched item: ", fetchedItem);
+      setPopoverMessage(false);
+    } else {
+      console.log("No items found");
+      setPopoverMessage(true);
+    }
   };
 
   function togglePopoverMessage() {
-    setPopoverMessage(!popoverMessage)
+    setPopoverMessage(!popoverMessage);
   }
-  
+
   const popover = (
-    <Popover id="popover-basic">
-      <Popover.Body>
+    <Popover id="popover-basic" style={styles.popover}>
+      <Popover.Body style={styles.popover}>
         No items found. Try searching for something else...
       </Popover.Body>
     </Popover>
   );
-
 
   return (
     <div>
@@ -52,7 +47,13 @@ const getAuctionItemByTitle = async () => {
           value={userInput}
           onChange={(e) => setUserInput(e.target.value)}
         />
-        <OverlayTrigger trigger="click" placement="right" overlay={popover} show={popoverMessage} onToggle={togglePopoverMessage}>
+        <OverlayTrigger
+          trigger="click"
+          placement="right"
+          overlay={popover}
+          show={popoverMessage}
+          onToggle={togglePopoverMessage}
+        >
           <button
             class="btn btn-outline-secondary my-2 my-sm-0"
             type="submit"
@@ -80,5 +81,10 @@ const styles = {
     color: "rgb(226, 89, 55)",
     borderColor: "rgb(226, 89, 55)",
     backgroundColor: "black",
+  },
+  popover: {
+    color: "rgb(226, 89, 55)",
+    backgroundColor: "black",
+   
   },
 };
