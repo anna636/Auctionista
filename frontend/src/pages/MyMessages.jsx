4ef8f8bd6e49548abb269fe5c03e36eead152f19
TiredBirb <72065138+anna636@users.noreply.github.com>
2021-11-01 16:57:39 +0100

    Added css

--- frontend/src/pages/MyMessages.jsx
+++ frontend/src/pages/MyMessages.jsx
@@ -1,14 +1,57 @@
 import React from "react";
+import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+} from "@chatscope/chat-ui-kit-react";
 
 
 function MyMessages() {
   
   return (
-    <div>
-      <h4>My messages</h4>
-      
+    <div className="chatWrapper" style={cosStyles.chatWrapper} >
+      <div className="people" style={cosStyles.people}>
+
+      </div>
+      <div style={{ position: "relative", height: "500px" }}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList>
+              <Message
+                model={{
+                  message: "Hello my friend",
+                  sentTime: "just now",
+                  sender: "Joe",
+                }}
+              />
+            </MessageList>
+            <MessageInput placeholder="Type message here" />
+          </ChatContainer>
+        </MainContainer>
+      </div>
     </div>
   );
 }
 
 export default MyMessages;
+
+
+const cosStyles = {
+  chatWrapper: {
+    display: "grid",
+    gridTemplateColumns: "30% 70%",
+    padding: "5vh 3vw 5vh 1vw",
+    gap: "1vw",
+    height: "100vh",
+    
+  },
+  people: {
+    width: "100%",
+    backgroundColor: "#c6e3fa",
+    borderRadius: "10px",
+    height: "87%",
+  },
+};
