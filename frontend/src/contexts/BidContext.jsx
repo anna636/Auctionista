dcc135e2581515db5d2b42066d2da3526e2f79ce
TiredBirb <72065138+anna636@users.noreply.github.com>
2021-11-08 18:40:32 +0100

    Fixed placing bid

--- frontend/src/contexts/BidContext.jsx
+++ frontend/src/contexts/BidContext.jsx
@@ -39,7 +39,10 @@ const BidProvider = (props) => {
     try {
       let response = await fetch("/rest/bids", {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: new Headers({
+          Authorization: "Bearer " + localStorage.getItem("accessToken"),
+          "Content-Type": "application/json",
+        }),
         body: JSON.stringify(itemToPost),
       });
       console.log(await response.json())
