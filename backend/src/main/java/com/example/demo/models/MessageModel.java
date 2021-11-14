19bea19ce5d649132a69410c181138d8bf8ed58e
TiredBirb <72065138+anna636@users.noreply.github.com>
2021-11-14 21:40:25 +0100

    Backend notif about being outbidden

--- backend/src/main/java/com/example/demo/models/MessageModel.java
+++ backend/src/main/java/com/example/demo/models/MessageModel.java
@@ -1,5 +1,12 @@
 package com.example.demo.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MessageModel {
 
     private String message;
--- backend/src/main/java/com/example/demo/services/BidService.java
+++ backend/src/main/java/com/example/demo/services/BidService.java
@@ -1,8 +1,10 @@
 package com.example.demo.services;
 
+import com.example.demo.controllers.MessageController;
 import com.example.demo.entities.AuctionItem;
 import com.example.demo.entities.Bid;
 import com.example.demo.entities.User;
+import com.example.demo.models.MessageModel;
 import com.example.demo.repositories.AuctionItemRepository;
 import com.example.demo.repositories.BidRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,9 @@ public class BidService {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private MessageController messageController;
+
     public List<Bid> getAll(){
         return bidRepository.findAll();
     }
@@ -35,8 +40,28 @@ public class BidService {
     public Bid saveBid(Bid bid){
 
         try{
+
+
+
+
+
             long auctionItemId = bid.getAuctionItem().getId();
             AuctionItem auctionItem = auctionItemRepository.getById(auctionItemId);
+
+
+            User user = userService.getById(Long.valueOf(bid.getUser_id())).get(); //Finding owner of user
+
+            //Building new message model to send
+            MessageModel notif=MessageModel.builder().message("bid owner: "+user.getUsername() + " item id "+ bid.getAuctionItem().getId())
+                    .type("notification").fromLogin(user.getEmail()).build();
+
+            //Getting user who will receive outbidden notification
+            Bid lastBid=auctionItem.getBids().get(auctionItem.getBids().size()-1);
+            User lastBidOwner=userService.getById(Long.valueOf(lastBid.getUser_id())).get();
+
+            //Sending notification
+            messageController.sendMessage(lastBidOwner.getEmail(), notif);
+
 
             if( Long.parseLong(bid.getUser_id()) == auctionItem.getOwner().getId()) {
                 System.out.println("User can't place bid on their own items");
