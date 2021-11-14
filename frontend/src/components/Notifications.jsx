c9b4073d70188b4a10df66bc0feae237eea4b3cd
nicolebradleey <nicole_bradley94@hotmail.com>
2021-11-14 15:05:05 +0100

    Functions for checking if bids and reservationprice have been met
    Co-Authored-By: Denise <72066356+DeniiseB@users.noreply.github.com>

--- frontend/src/components/Notifications.jsx
+++ frontend/src/components/Notifications.jsx
@@ -26,41 +26,65 @@ function Notifications() {
     let bids = await fetchBidsByUserId(currentUser.id);
     console.log(bids);
     if (bids && bids.length) {
-      let auctionItemsWithDuplicates = []
+      let auctionItemsWithDuplicates = [];
       for (let bid of bids) {
-        // CHANGE FALSE TO TRUE AFTER TESTING *************
-        if (bid.auctionItem.sold === false)
-        {
-          auctionItemsWithDuplicates.push(bid.auctionItem)
-        }
-      }     
-      let auctionItems = removeDuplicates(auctionItemsWithDuplicates)
-      return auctionItems
-    }
+        auctionItemsWithDuplicates.push(bid.auctionItem);
+      }
+      let noDuplicates = removeDuplicates(auctionItemsWithDuplicates);
+      let checkedDeadline = checkDeadline(noDuplicates);
+      let checkedReservationPrice = checkReservationPrice(checkedDeadline);
 
+      return checkedReservationPrice;
+    }
   };
+
+  function checkDeadline(auctionItems) {
+    let deadlineReachedAuctionItems = [];
+
+    if (auctionItems && auctionItems.length) {
+      for (let item of auctionItems) {
+        let now = new Date();
+        let itemDeadline = new Date(item.deadline);
+        let minusTime = now - itemDeadline;
+
+        if (minusTime > 0) {
+          deadlineReachedAuctionItems.push(item);
+        }
+      }
+    }
+    return deadlineReachedAuctionItems;
+  }
+
+  function checkReservationPrice(auctionItems) {
+    let reservationPriceReached = [];
+
+    if (auctionItems && auctionItems.length) {
+      for (let item of auctionItems) {
+        if (item.bids && item.bids.length) {
+          if (item.reservationPrice <= item.bids[item.bids.length - 1].amount) {
+            reservationPriceReached.push(item);
+          }
+        }
+      }
+    }
+    return reservationPriceReached;
+  }
 
   function removeDuplicates(arrayWithDuplicates) {
     // Using IDs because each bid item is unique
     let arrayWithDuplicatesIDs = [];
-    arrayWithDuplicates.map((item) =>
-      arrayWithDuplicatesIDs.push(item.id)
-    );
+    arrayWithDuplicates.map((item) => arrayWithDuplicatesIDs.push(item.id));
     // Converting into Set and back again removes duplicates
     let cleanArrayIDs = [...new Set(arrayWithDuplicatesIDs)];
     let cleanArray = [];
 
     for (let id of cleanArrayIDs) {
-      let auctionItem = arrayWithDuplicates.find(
-        (item) => item.id === id
-      );
+      let auctionItem = arrayWithDuplicates.find((item) => item.id === id);
       cleanArray.push(auctionItem);
     }
     console.log(cleanArray);
     return cleanArray;
   }
-
-
 
   return (
     <div style={styles.box}>
